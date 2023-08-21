22fccbf964004358a716575e931f5de48a79386f
luizantonioo <125517343+luizantonioo@users.noreply.github.com>
2023-08-21 17:47:58 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -27,6 +27,12 @@ function speak(){
     speak_data = "Tirando sua selfie nos próximos 5 segundos";
     var utterThis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
+setTimeout()
+var img_id, id="selfie1" ;
+function take_snapshot(){
+speak_data = "tirando sua selfie em 5 segundos";
+//para professora:( -->
+// nao faço ideia de como fazer os outros passos prof  nao faz sentido ter varias funçoes dentro de uma só sera que vc pode me ajudar na aula?
 
-
+    }
 }
