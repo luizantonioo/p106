8449b2a6d11fc36666cc21914ea2ee24b9601ad4
luizantonioo <125517343+luizantonioo@users.noreply.github.com>
2023-08-21 17:35:10 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var Content;
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    recognition.start();
+} 
+
+
+camera = document.getElementById("camera");
+Webcam.set({
+    width:500,
+    height:400,
+    image_format : 'jpeg',
+    jpeg_quality:90
+});
+
+
+
+function speak(){
+
+    
+    var synth = window.speechSynthesis;
+    Webcam.attach(camera);
+
+    speak_data = "Tirando sua selfie nos próximos 5 segundos";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+
+
+}
